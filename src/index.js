94d4190ff7c46139ee6f680b6d99267b8441e495
Tao <werewaswas@live.com>
2018-06-18 02:16:48 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from './store';
+import bandReducer from './reducers/band_reducer'
+
+const store = createStore(bandReducer);
 
 function render(){
-  ReactDOM.render(<InsertTopLevelComponent />, document.getElementById('container'))
+  ReactDOM.render(<BandInput store={store} />, document.getElementById('container'))
 }
